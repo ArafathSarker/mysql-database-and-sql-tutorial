fab123551b9f2b2020b6261cd4f16ee4c5631567
arafath <arafathmd432@gmail.com>
2025-10-13 13:54:15 +0600

    Tutorial-6 done

---
+++ Query/Tutorial-6/How_to_comment/How_to_comment.sql
@@ -0,0 +1,16 @@
+-- We can comment in sql (--) this is called single line comment
+
+/*
+This 
+is a
+multiple line
+comment.
+*/
+
+CREATE DATABASE Human;
+
+-- This line of text will not affect your query
+
+
+
+
---
+++ Query/Tutorial-6/Rename_a_table/Rename_a_table.sql
@@ -0,0 +1,8 @@
+-- We can reaname a table name
+
+-- RENAME TABLE old_name TO new_name
+
+RENAME TABLE student TO student_table;
+
+
+
