bc2dcd842d362c3d2cfb8c87a9c4eda8d234afe9
arafath <arafathmd432@gmail.com>
2025-10-13 10:31:22 +0600

    create multiple database with multiple query

---
+++ Query/Create_Database/create_multiple_database.sql
@@ -0,0 +1,8 @@
+-- We can run multiple queries at a time
+-- Here we have to create three database at a time 
+
+CREATE DATABASE Teacher;
+CREATE DATABASE Student;
+CREATE DATABASE Medical;
+
+-- Here Teacher, Student and Medical three database will create
